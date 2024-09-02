531bf6ee79493c49ebc62b500332549be0937021
sALT <93945502+saltern@users.noreply.github.com>
2024-09-02 00:01:12 -0300

    Fix cell index spinbox carryover

--- SCENES/TABS/SHARED/CELLS/cell_index.gd
+++ SCENES/TABS/SHARED/CELLS/cell_index.gd
@@ -1,10 +1,16 @@
 extends SpinBox
 
+var obj_state: ObjectEditState
+
 
 func _ready() -> void:
-	var obj_state: ObjectEditState = SessionData.object_state_get(
-		get_owner().get_parent().name)
+	obj_state = SessionData.object_state_get(get_owner().get_parent().name)
 	
 	value_changed.connect(obj_state.cell_load)
-
+	obj_state.cell_updated.connect(update)
+	
 	max_value = obj_state.data.cells.size() - 1
+
+
+func update(cell: Cell) -> void:
+	call_deferred("set_value_no_signal", obj_state.cell_index)
