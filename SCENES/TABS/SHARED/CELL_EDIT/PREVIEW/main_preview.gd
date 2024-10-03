28921a32f3fc17cc8377b0ea7937cc7df4bea059
sALT <93945502+saltern@users.noreply.github.com>
2024-10-03 17:14:53 -0300

    Standalone and extendable zoom+pan

---
+++ SCENES/TABS/SHARED/CELL_EDIT/PREVIEW/main_preview.gd
@@ -0,0 +1,51 @@
+extends "res://SCENES/TABS/SHARED/zoomable_preview.gd"
+
+@export var box_parent: Control
+
+var sprite_drag: bool = false
+var tentative: bool = false
+
+@onready var cell_edit: CellEdit = get_owner()
+
+
+func mouse_button(event: InputEventMouseButton) -> void:
+	match event.button_index:
+		MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				if cell_edit.box_drawing_mode:
+					box_parent.start_drawing(
+						event.position - visualizer.position)
+				
+				elif event.alt_pressed:
+					sprite_drag = true
+				
+				else:
+					tentative = true
+			
+			else:
+				if sprite_drag:
+					sprite_drag = false
+					accum = Vector2.ZERO
+				
+				elif tentative:
+					cell_edit.box_deselect_all()
+					tentative = false
+				
+				box_parent.stop_drawing(
+					event.position - visualizer.position, true)
+
+
+func mouse_motion(event: InputEventMouseMotion) -> void:
+	if sprite_drag:
+		var mov_scale: float = visualizer.scale.x
+		var scaled_relative: Vector2 = event.relative / mov_scale
+		accum += scaled_relative
+		var accum_floor := Vector2(floor(accum.x), floor(accum.y))
+		
+		cell_edit.sprite_set_position(
+			cell_edit.this_cell.sprite_info.position +
+			(accum_floor as Vector2i))
+		
+		accum -= accum_floor
+	
+	box_parent.drag(event.position - visualizer.position)
---
+++ SCENES/TABS/SHARED/zoomable_preview.gd
@@ -0,0 +1,61 @@
+extends PanelContainer
+
+@export var visualizer: Node2D
+
+var panning: bool = false
+var accum: Vector2
+
+
+func _process(_delta: float) -> void:
+	queue_redraw()
+
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if not handle_zoom_pan(event):
+			mouse_button(event as InputEventMouseButton)
+	
+	elif event is InputEventMouseMotion:
+		if not handle_pan_drag(event):
+			mouse_motion(event as InputEventMouseMotion)
+
+
+func handle_zoom_pan(event: InputEventMouseButton) -> bool:
+	match event.button_index:
+		MOUSE_BUTTON_WHEEL_UP:
+			if event.pressed && event.is_command_or_control_pressed():
+				visualizer.zoom_in()
+				get_viewport().set_input_as_handled()
+				return true
+	
+		MOUSE_BUTTON_WHEEL_DOWN:
+			if event.pressed && event.is_command_or_control_pressed():
+				visualizer.zoom_out()
+				get_viewport().set_input_as_handled()
+				return true
+		
+		MOUSE_BUTTON_RIGHT:
+			if event.pressed:
+				panning = true
+				return true
+			else:
+				panning = false
+				return true
+	
+	return false
+
+
+func handle_pan_drag(event: InputEventMouseMotion) -> bool:
+	if panning:
+		visualizer.position += event.relative
+		return true
+	
+	return false
+
+
+func mouse_button(event: InputEventMouseButton) -> void:
+	pass
+
+
+func mouse_motion(event: InputEventMouseMotion) -> void:
+	pass
