36a945f5b6bc1c3cf2d01a79c7589981267c564b
sALT <93945502+saltern@users.noreply.github.com>
2024-10-18 14:56:41 -0300

    Ignore "unused" signals in GlobalSignals

--- AUTOLOADS/global_signals.gd
+++ AUTOLOADS/global_signals.gd
@@ -1,10 +1,17 @@
 extends Node
 
+@warning_ignore("unused_signal")
 signal menu_undo
+@warning_ignore("unused_signal")
 signal menu_redo
 
+@warning_ignore("unused_signal")
 signal menu_save	# Emitted by File PopupMenu, used by character_tab_list.gd
+@warning_ignore("unused_signal")
 signal save_start
+@warning_ignore("unused_signal")
 signal save_object
+@warning_ignore("unused_signal")
 signal save_sub_object
+@warning_ignore("unused_signal")
 signal save_complete
