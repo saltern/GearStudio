d803dec09199f0aa4c87fb2c4ffbde452203233f
sALT <93945502+saltern@users.noreply.github.com>
2024-10-20 15:44:44 -0300

    Allow BoxInfo duplication

--- CLASSES/RESOURCE/class_box_info.gd
+++ CLASSES/RESOURCE/class_box_info.gd
@@ -1,6 +1,6 @@
 class_name BoxInfo extends Resource
 
-var rect: Rect2i = Rect2i(0, 0, 0, 0)
-var type: int = 0
-var crop_x_offset: int = 0
-var crop_y_offset: int = 0
+@export_storage var rect: Rect2i = Rect2i(0, 0, 0, 0)
+@export_storage var type: int = 0
+@export_storage var crop_x_offset: int = 0
+@export_storage var crop_y_offset: int = 0
