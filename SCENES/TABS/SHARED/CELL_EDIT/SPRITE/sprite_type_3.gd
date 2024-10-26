6e9d47de896e00c29696831c01ed3eeb1d04297f
sALT <93945502+saltern@users.noreply.github.com>
2024-10-26 00:29:12 -0300

    Add a preview for type 3/6 boxes

---
+++ SCENES/TABS/SHARED/CELL_EDIT/SPRITE/sprite_type_3.gd
@@ -0,0 +1,18 @@
+extends TextureRect
+
+@onready var cell_edit: CellEdit = owner
+
+
+func _ready() -> void:
+	cell_edit.cell_updated.connect(cell_loaded)
+	cell_edit.box_deselected_all.connect(hide)
+	cell_edit.box_selected.connect(select_check)
+
+
+func cell_loaded(cell: Cell) -> void:
+	texture = cell_edit.obj_data.sprite_get(cell.sprite_info.index).texture
+
+
+func select_check(box: BoxInfo) -> void:
+	if box.type == 3 or box.type == 6:
+		show()
