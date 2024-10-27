92c59415dcf07af13b7f39c801870d8d94b8baf9
sALT <93945502+saltern@users.noreply.github.com>
2024-10-27 15:19:38 -0300

    Remove unnecesary material dupe
    Resolves type 3 box sprite preview not working on objects

--- CLASSES/CONTROL/class_cell_sprite_display.gd
+++ CLASSES/CONTROL/class_cell_sprite_display.gd
@@ -7,9 +7,6 @@ var palette_index: int = 0
 
 
 func _enter_tree() -> void:
-	if get_owner().get_parent().name != "player":
-		material = material.duplicate()
-	
 	texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
 
 
