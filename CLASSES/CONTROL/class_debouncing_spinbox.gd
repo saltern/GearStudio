86a88bac79b161f13956aa6296d7d7b3f393b9b5
sALT <93945502+saltern@users.noreply.github.com>
2024-12-31 18:23:37 -0300

    Add DebouncingSpinBox

---
+++ CLASSES/CONTROL/class_debouncing_spinbox.gd
@@ -0,0 +1,36 @@
+class_name DebouncingSpinBox extends SteppingSpinBox
+
+signal drag_ended
+signal value_set
+signal value_preview
+
+var start_value: int = 0
+var dragging: bool = false
+
+
+func _ready() -> void:
+	value_changed.connect(drag_check)
+
+
+func _gui_input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton:
+		return
+	
+	match event.button_index:
+		MOUSE_BUTTON_LEFT:
+			if not event.pressed:
+				if dragging && start_value != value:
+					drag_ended.emit(value)
+				
+				dragging = false
+				
+			else:
+				start_value = value
+				dragging = true
+
+
+func drag_check(new_value: int) -> void:
+	if dragging:
+		return
+	
+	value_set.emit(new_value)
