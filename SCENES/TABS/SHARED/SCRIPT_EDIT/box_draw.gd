a290e26cd5b1e2bdb08cf518e29300992d78e1ab
sALT <93945502+saltern@users.noreply.github.com>
2024-12-31 18:19:23 -0300

    Tie box preview scale to sprite, disable rotation

--- SCENES/TABS/SHARED/SCRIPT_EDIT/box_draw.gd
+++ SCENES/TABS/SHARED/SCRIPT_EDIT/box_draw.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var box_display_toggle: CheckBox
+@export var box_display_toggle: CheckButton
 @export var sprite_handler: Control
 
 var box_list: Array[BoxInfo]
@@ -12,14 +12,14 @@ var scale_factor: Vector2 = Vector2(1.0, 1.0)
 var angle: int = 0
 
 @onready var script_edit: ScriptEdit = owner
+@onready var draw_offset: Vector2 = -position
 
 
 func _ready() -> void:
 	box_display_toggle.toggled.connect(toggle_display)
 	script_edit.cell_clear.connect(clear_boxes)
 	script_edit.inst_cell.connect(on_cell)
-	script_edit.inst_scale.connect(on_scale)
-	script_edit.inst_rotate.connect(on_rotate)
+	sprite_handler.scale_set.connect(on_scale)
 
 
 func _process(_delta: float) -> void:
@@ -37,33 +37,46 @@ func _draw():
 		
 		var color := Settings.box_colors[box.box_type]
 		
+		var top: float = box.y_offset * scale_factor.y
+		top += draw_offset.y
+		
+		var left: float = box.x_offset * scale_factor.x
+		left += draw_offset.x
+		
+		var right: float = (box.x_offset + box.width) * scale_factor.x
+		right -= Settings.box_thickness
+		right += draw_offset.x
+		
+		var bottom: float = (box.y_offset + box.height) * scale_factor.y
+		bottom -= Settings.box_thickness
+		bottom += draw_offset.y
+		
+		var horz: float = box.width * scale_factor.x
+		var vert: float = box.height * scale_factor.y
+		
 		# Upper
 		draw_rect(Rect2(
-				Vector2(box.x_offset, box.y_offset) * scale_factor,
-				Vector2i(box.width * scale_factor.x, Settings.box_thickness)),
-			color)
-		
-		# Lower
-		draw_rect(Rect2(
-				Vector2(
-					box.x_offset * scale_factor.x,
-					((box.y_offset + box.height) * scale_factor.y) - Settings.box_thickness),
-				Vector2i(box.width * scale_factor.x, Settings.box_thickness)),
-			color)
+				Vector2(left, top), Vector2i(horz, Settings.box_thickness)
+			), color
+		)
 		
 		# Left
 		draw_rect(Rect2(
-				Vector2(box.x_offset, box.y_offset) * scale_factor,
-				Vector2i(Settings.box_thickness, box.height * scale_factor.y)),
-			color)
+				Vector2(left, top), Vector2i(Settings.box_thickness, vert)
+			), color
+		)
+		
+		# Lower
+		draw_rect(Rect2(
+				Vector2(left, bottom), Vector2i(horz, Settings.box_thickness)
+			), color
+		)
 		
 		# Right
 		draw_rect(Rect2(
-				Vector2(
-					((box.x_offset + box.width) * scale_factor.x) - Settings.box_thickness,
-					box.y_offset * scale_factor.y),
-				Vector2i(Settings.box_thickness, box.height * scale_factor.y)),
-			color)
+				Vector2(right, top), Vector2i(Settings.box_thickness, vert)
+			), color
+		)
 
 
 func toggle_display(enabled: bool) -> void:
@@ -95,32 +108,7 @@ func on_cell(index: int) -> void:
 	on_cell_update(cell)
 
 
-func on_scale(mode: int, value: int) -> void:
-	match mode:
-		0:
-			scale_x = value
-		1:
-			scale_y = value
-		2:
-			scale_x += value
-		3:
-			scale_y += value
-		4:
-			# ???
-			scale_x += randi_range(0, pow(256, 4) - 1) & value
-			pass #scale = scale + (rng AND value)
-		5:
-			# ???
-			scale_y += randi_range(0, pow(256, 4) - 1) & value
-			pass #scaleY = scaleY + (rng AND value)
-		6:
-			# ???
-			scale_x = (scale_x * float(value) / 100.00)
-		7:
-			# ???
-			scale_y = (scale_y * float(value) / 100.00)
-	
-	# Apply
+func on_scale(scale_x: float, scale_y: float) -> void:
 	if scale_x == -1:
 		scale_factor.x = 1.0
 	else:
@@ -130,27 +118,4 @@ func on_scale(mode: int, value: int) -> void:
 		scale_factor.y = scale_factor.x
 	else:
 		scale_factor.y = float(scale_y) / 1000.00
-
-
-func on_rotate(mode: int, value: int) -> void:
-	match mode:
-		0:
-			angle = value
-		1:
-			if value == 0:
-				angle = (angle & 0xFF) << 8
-			else:
-				angle = randi_range(0, pow(256, 4) - 1) & value
-		2:
-			angle += value
-		3:
-			# Unknown at this time
-			# angle = localid * 256
-			pass
-		4:
-			angle += randi_range(0, pow(256, 4) - 1) & value
-		5:
-			angle += randi_range(0, pow(256, 4) - 1) % value
-	
-	rotation_degrees = (365.0 / pow(256, 2)) * angle
 #endregion
