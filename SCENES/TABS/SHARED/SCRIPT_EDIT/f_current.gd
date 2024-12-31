48ffdb17579cb19b14ebd7307e5bf2b03ae80390
sALT <93945502+saltern@users.noreply.github.com>
2024-12-31 18:18:44 -0300

    Update frame counter on external seek

--- SCENES/TABS/SHARED/SCRIPT_EDIT/f_current.gd
+++ SCENES/TABS/SHARED/SCRIPT_EDIT/f_current.gd
@@ -2,8 +2,11 @@ extends Label
 
 @export var timeline: HSlider
 
+@onready var script_edit: ScriptEdit = owner
+
 
 func _ready() -> void:
+	script_edit.action_seek_to_frame.connect(update)
 	timeline.value_changed.connect(update)
 
 
