3f98f44036dfd51a7d300195d7e868357b8ca76c
sALT <93945502+saltern@users.noreply.github.com>
2025-01-02 02:03:00 -0300

    Allow external updates to action index spinbox

--- SCENES/TABS/SHARED/SCRIPT_EDIT/action_index.gd
+++ SCENES/TABS/SHARED/SCRIPT_EDIT/action_index.gd
@@ -4,9 +4,19 @@ extends SteppingSpinBox
 
 
 func _ready() -> void:
-	max_value = script_edit.bin_script.actions.size() - 1
 	value_changed.connect(load_action)
+	script_edit.action_count_changed.connect(update_max)
+	script_edit.action_force_select.connect(on_external_select)
+	update_max()
 
 
 func load_action(index: int) -> void:
-	script_edit.script_load_action(index)
+	script_edit.script_action_load(index)
+
+
+func update_max() -> void:
+	max_value = script_edit.script_action_get_count() - 1
+
+
+func on_external_select(new_value: int) -> void:
+	set_value_no_signal(new_value)
