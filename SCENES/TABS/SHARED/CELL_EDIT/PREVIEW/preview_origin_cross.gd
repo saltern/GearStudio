3b2d9cfae69372f5bf3aafa5ae5108b3a31eb7d8
sALT <93945502+saltern@users.noreply.github.com>
2025-03-03 18:25:09 -0300

    Add one more origin cross type

--- SCENES/TABS/SHARED/CELL_EDIT/PREVIEW/preview_origin_cross.gd
+++ SCENES/TABS/SHARED/CELL_EDIT/PREVIEW/preview_origin_cross.gd
@@ -12,27 +12,34 @@ func _draw() -> void:
 		0:	# BG
 			draw_rect(Rect2(-1, -21, 3, 43), Color.BLACK)
 			draw_rect(Rect2(-21, -1, 43, 3), Color.BLACK)
+			draw_solid_cross()
 		1:	# BG
 			draw_rect(Rect2(-1, -21, 3, 17), Color.BLACK)
 			draw_rect(Rect2(-1, 5, 3, 17), Color.BLACK)
 			draw_rect(Rect2(-21, -1, 17, 3), Color.BLACK)
 			draw_rect(Rect2(5, -1, 17, 3), Color.BLACK)
+			draw_crosshair()
 		2:	# No BG necessary here
 			pass
-		_:
+			draw_solid_cross()
+		3:
 			draw_rect(Rect2(-5, 0, 11, 1), Color.BLACK)
 			draw_rect(Rect2(0, -5, 1, 11), Color.BLACK)
-	
-	# FG
-	match Settings.cell_origin_type % 2:
-		0:
-			draw_rect(Rect2(0, -20, 1, 41), Color.WHITE)
-			draw_rect(Rect2(-20, 0, 41, 1), Color.WHITE)
-		1:
-			draw_rect(Rect2(0, -20, 1, 15), Color.WHITE)
-			draw_rect(Rect2(0, 6, 1, 15), Color.WHITE)
-			draw_rect(Rect2(-20, 0, 15, 1), Color.WHITE)
-			draw_rect(Rect2(6, 0, 15, 1), Color.WHITE)
+			draw_crosshair()
+		_:
+			draw_crosshair()
+
+
+func draw_solid_cross() -> void:
+	draw_rect(Rect2(0, -20, 1, 41), Color.WHITE)
+	draw_rect(Rect2(-20, 0, 41, 1), Color.WHITE)
+
+
+func draw_crosshair() -> void:
+	draw_rect(Rect2(0, -20, 1, 15), Color.WHITE)
+	draw_rect(Rect2(0, 6, 1, 15), Color.WHITE)
+	draw_rect(Rect2(-20, 0, 15, 1), Color.WHITE)
+	draw_rect(Rect2(6, 0, 15, 1), Color.WHITE)
 
 
 func update_visibility() -> void:
