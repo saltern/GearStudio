42a58fed6ed05fc3ed43812803757882c1a78b74
sALT <93945502+saltern@users.noreply.github.com>
2025-04-06 01:13:24 -0300

    Make palette spinbox portable, add to ScriptEdit

--- SCENES/TABS/SHARED/PREVIEW/palette_index.gd
+++ SCENES/TABS/SHARED/PREVIEW/palette_index.gd
@@ -1,21 +1,21 @@
 extends SteppingSpinBox
 
-@onready var cell_edit: CellEdit = get_owner()
+@onready var provider: Node = get_owner()
 
 
 func _ready() -> void:
-	if not cell_edit.obj_data.has("palettes"):
+	if not provider.obj_data.has("palettes"):
 		get_parent().hide()
 		return
 	
-	max_value = cell_edit.obj_data["palettes"].size() - 1
+	max_value = provider.obj_data["palettes"].size() - 1
 	
 	value_changed.connect(SessionData.set_palette)
 	SessionData.palette_changed.connect(external_set_palette)
 
 
 func external_set_palette(for_session: int, index: int) -> void:
-	if for_session != cell_edit.session_id:
+	if for_session != provider.session_id:
 		return
 	
 	set_value_no_signal.call_deferred(index)
