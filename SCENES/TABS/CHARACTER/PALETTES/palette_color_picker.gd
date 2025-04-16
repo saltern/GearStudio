47e3f01718b42bb08db9dfa7895933456fbacf65
sALT <93945502+saltern@users.noreply.github.com>
2025-04-16 14:07:02 -0300

    Update color picker on palette change undo

--- SCENES/TABS/CHARACTER/PALETTES/palette_color_picker.gd
+++ SCENES/TABS/CHARACTER/PALETTES/palette_color_picker.gd
@@ -1,11 +1,18 @@
 extends ColorPicker
 
 var provider: PaletteProvider
+var last_index: int
 
 
 func _ready() -> void:
+	SessionData.palette_changed.connect(update_palette.unbind(2))
 	provider = get_owner().get_provider()
 
 
 func on_color_selected(index: int) -> void:
+	last_index = index
 	color = provider.palette_get_color(index)
+
+
+func update_palette() -> void:
+	on_color_selected(last_index)
