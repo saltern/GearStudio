a539ca21029496089fafdf3937f9a07c15e6541f
sALT <93945502+saltern@users.noreply.github.com>
2025-06-22 00:27:36 -0300

    Use textures for origin cross

--- SCENES/TABS/SHARED/PREVIEW/preview_origin_cross.gd
+++ SCENES/TABS/SHARED/PREVIEW/preview_origin_cross.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var texture: ImageTexture
+
 
 func _ready() -> void:
 	Settings.draw_origin_changed.connect(update_visibility)
@@ -8,38 +10,10 @@ func _ready() -> void:
 
 
 func _draw() -> void:
-	match Settings.cell_origin_type:
-		0:	# BG
-			draw_rect(Rect2(-1, -21, 3, 43), Color.BLACK)
-			draw_rect(Rect2(-21, -1, 43, 3), Color.BLACK)
-			draw_solid_cross()
-		1:	# BG
-			draw_rect(Rect2(-1, -21, 3, 17), Color.BLACK)
-			draw_rect(Rect2(-1, 5, 3, 17), Color.BLACK)
-			draw_rect(Rect2(-21, -1, 17, 3), Color.BLACK)
-			draw_rect(Rect2(5, -1, 17, 3), Color.BLACK)
-			draw_crosshair()
-		2:	# No BG necessary here
-			pass
-			draw_solid_cross()
-		3:
-			draw_rect(Rect2(-5, 0, 11, 1), Color.BLACK)
-			draw_rect(Rect2(0, -5, 1, 11), Color.BLACK)
-			draw_crosshair()
-		_:
-			draw_crosshair()
-
-
-func draw_solid_cross() -> void:
-	draw_rect(Rect2(0, -20, 1, 41), Color.WHITE)
-	draw_rect(Rect2(-20, 0, 41, 1), Color.WHITE)
-
-
-func draw_crosshair() -> void:
-	draw_rect(Rect2(0, -20, 1, 15), Color.WHITE)
-	draw_rect(Rect2(0, 6, 1, 15), Color.WHITE)
-	draw_rect(Rect2(-20, 0, 15, 1), Color.WHITE)
-	draw_rect(Rect2(6, 0, 15, 1), Color.WHITE)
+	texture = Settings.get_origin_texture()
+	var x: float = - floor(texture.get_width() / 2)
+	var y: float = - floor(texture.get_height() / 2)
+	draw_texture(texture, Vector2(x, y))
 
 
 func update_visibility() -> void:
