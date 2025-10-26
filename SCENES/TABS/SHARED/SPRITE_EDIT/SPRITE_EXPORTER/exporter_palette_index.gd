adfc2762f44bfc1bae59eda001b2f23471cde223
sALT <93945502+saltern@users.noreply.github.com>
2025-10-26 12:10:20 -0300

    Fix OOB palette selection on sprite export
    Check the object's actual palette count and apply limit to sprite exporter

--- SCENES/TABS/SHARED/SPRITE_EDIT/SPRITE_EXPORTER/exporter_palette_index.gd
+++ SCENES/TABS/SHARED/SPRITE_EDIT/SPRITE_EXPORTER/exporter_palette_index.gd
@@ -4,6 +4,10 @@ extends SteppingSpinBox
 func _ready() -> void:
 	visibility_changed.connect(update)
 	value_changed.connect(update.unbind(1))
+	
+	var session_id: int = get_owner().session_id
+	var palette_count: int = SessionData.session_get_palettes(session_id).size()
+	max_value = palette_count - 1
 
 
 func update() -> void:
