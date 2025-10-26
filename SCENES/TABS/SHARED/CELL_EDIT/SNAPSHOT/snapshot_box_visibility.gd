a5d914dea454d11950d7b9dad64ba0494308d052
sALT <93945502+saltern@users.noreply.github.com>
2025-10-26 00:14:08 -0300

    Match box toggle in snapshot dialog to cell editor
    Syncs the visibility CheckButton between the cell editor and the multi snapshot dialog when the cell editor's is toggled.

--- SCENES/TABS/SHARED/CELL_EDIT/SNAPSHOT/snapshot_box_visibility.gd
+++ SCENES/TABS/SHARED/CELL_EDIT/SNAPSHOT/snapshot_box_visibility.gd
@@ -5,7 +5,12 @@ extends CheckButton
 
 func _ready() -> void:
 	button_pressed = main_button.button_pressed
+	main_button.toggled.connect(toggle_no_signal)
 
 
 func _toggled(toggled_on: bool) -> void:
 	main_button.button_pressed = toggled_on
+
+
+func toggle_no_signal(toggled_on: bool) -> void:
+	set_pressed_no_signal(toggled_on)
