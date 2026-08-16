44c285fecd8deddf167e866b30f7b83f3b69e05b
sALT <93945502+saltern@users.noreply.github.com>
2026-08-16 11:46:09 -0300

    BinSprite done question mark

--- CLASSES/RESOURCE/BINARIES/bin_sprite.gd
+++ CLASSES/RESOURCE/BINARIES/bin_sprite.gd
@@ -96,18 +96,254 @@ const IMAGE_FORMAT			: Image.Format = Image.Format.FORMAT_L8
 # Variables
 var mode			: Mode
 var clut			: CLUT
-var bit_depth		: int
+var bit_depth		: int:
+	get:
+		if bit_depth == DEPTH_4:
+			return DEPTH_4
+		else:
+			return DEPTH_8
+	set(value):
+		if value == DEPTH_4:
+			bit_depth = DEPTH_4
+		else:
+			bit_depth = DEPTH_8
+
 var width			: int
 var height			: int
-var texture_width	: int
-var texture_height	: int
+
+var texture_width	: int:
+	get:
+		return pow(2, texture_width)
+		
+var texture_height	: int:
+	get:
+		return pow(2, texture_height)
+
 var id_hash			: int
 var manual_hash		: bool
-var palette			: PackedByteArray
+var palette			: PackedByteArray:
+	get:
+		if clut == CLUT.NONE:
+			return []
+		else:
+			return palette
+	set(value):
+		value.resize(COLOR_SIZE * get_color_count())
+		palette = value
+	
 var pixels			: PackedByteArray
+
 # Not serialized
 var image			: Image
 var texture			: ImageTexture
+
+
+static func identify(bin_data: PackedByteArray) -> bool:
+	if GGXP_MODES.has(bin_data[ADDRESS_MODE]):
+		if !GGXP_COMPRESSION.has(bin_data[ADDRESS_GGXP_CC] >> 0x4):
+			return false
+		if !GGXP_CLUT.has(bin_data[ADDRESS_GGXP_CC] & 0xF):
+			return false
+		
+		return true
+	
+	var stream: StreamPeerBuffer = StreamPeerBuffer.new()
+	stream.data_array = bin_data
+	
+	var bin_mode: int = stream.get_u16()
+	var bin_clut: int = stream.get_u16()
+	var bin_bpp: int = stream.get_u16()
+	
+	if !COMMON_MODES.has(bin_mode)	: return false
+	if !COMMON_CLUT.has(bin_clut)		: return false
+	if !COMMON_DEPTH.has(bin_bpp)		: return false
+	
+	return true
+
+
+func serialize() -> PackedByteArray:
+	var stream: StreamPeerBuffer = StreamPeerBuffer.new()
+
+	match mode:
+		Mode.RAW:
+			stream.put_u16(MODE_RAW)
+		Mode.PALETTE:
+			stream.put_u16(MODE_PALETTE)
+		_:
+			stream.put_u16(MODE_ACPR)
+
+	match clut:
+		CLUT.NONE:
+			stream.put_u16(CLUT_NONE)
+		CLUT.HALF:
+			stream.put_u16(CLUT_HALF)
+		CLUT.FULL:
+			stream.put_u16(CLUT_FULL)
+
+	# Depth
+	stream.put_u16(bit_depth)
+
+	# Width, height
+	stream.put_u16(width)
+	stream.put_u16(height)
+
+	# Allocated texture width, height
+	stream.put_u16(texture_width)
+	stream.put_u16(texture_height)
+
+	# Hash
+	stream.put_u16(id_hash)
+
+	# Palette
+	if clut != CLUT.NONE:
+		stream.put_data(palette)
+
+	# Pixel data
+	match mode:
+		Mode.RAW:
+			stream.put_data(pixels)
+		Mode.PALETTE:
+			pass
+		_:
+			stream.put_data(SpriteCompression.compress_acpr(pixels, bit_depth))
+
+	return stream.data_array
+
+
+func deserialize(bin_data: PackedByteArray, is_big_endian: bool) -> void:
+	# Assumes this has already been identified as a sprite
+	var ggxp_compressed: bool = false
+	var pal_size: int
+	var stream: StreamPeerBuffer = StreamPeerBuffer.new()
+	stream.data_array = bin_data
+	stream.big_endian = is_big_endian
+
+	var bin_mode: int = stream.get_u8()
+
+	# GGX Plus sprites
+	if GGXP_MODES.has(bin_mode):
+		mode = Mode.GGXP
+
+		match bin_mode:
+			MODE_GGXP4:
+				bit_depth = DEPTH_4
+			MODE_GGXP8:
+				bit_depth = DEPTH_8
+			_:
+				push_error("BinSprite::deserialize() error! Invalid GGX Plus mode")
+				return
+
+		var ggxp_cc: int = stream.get_u8()
+
+		# High nibble: compression value
+		match ggxp_cc >> 4:
+			GGXP_UNCOMPRESSED:
+				ggxp_compressed = false
+			GGXP_COMPRESSED:
+				ggxp_compressed = true
+			_:
+				push_error("BinSprite::deserialize() error! Invalid GGX Plus compression")
+				return
+
+		# Low nibble: CLUT value
+		match ggxp_cc & 0xF:
+			GGXP_CLUT_NONE:
+				clut = CLUT.NONE
+			GGXP_CLUT_HALF:
+				clut = CLUT.HALF
+			GGXP_CLUT_FULL:
+				clut = CLUT.FULL
+			_:
+				push_error("BinSprite::deserialize() error! Invalid GGX Plus CLUT")
+				return
+
+		width = stream.get_u16()
+		height = stream.get_u16()
+
+		# Texture size
+		texture_width = get_texture_size(width)
+		texture_height = get_texture_size(height)
+
+		# Hash, palette
+		pal_size = COLOR_SIZE * pow(2, bit_depth)
+
+		match clut:
+			CLUT.NONE:
+				pal_size = 0
+			CLUT.HALF:
+				pal_size /= 2
+			_:
+				pass
+
+		id_hash = hash(bin_data.slice(ADDRESS_HEADER_END + pal_size, bin_data.size()))
+
+	# The rest
+	else:
+		stream.seek(ADDRESS_CLUT)
+
+		match bin_mode:
+			MODE_RAW:
+				mode = Mode.RAW
+			MODE_ACPR:
+				mode = Mode.ACPR
+			MODE_PALETTE:
+				mode = Mode.PALETTE
+			MODE_5:
+				mode = Mode.MODE_5
+			_:
+				push_error("BinSprite::deserialize() error! Invalid mode")
+				return
+
+		match stream.get_u16():
+			CLUT_NONE:
+				clut = CLUT.NONE
+			CLUT_HALF:
+				clut = CLUT.HALF
+			CLUT_FULL:
+				clut = CLUT.FULL
+			_:
+				push_error("BinSprite::deserialize() error! Invalid CLUT value")
+				return
+
+		bit_depth = stream.get_u16()
+		width = stream.get_u16()
+		height = stream.get_u16()
+		texture_width = stream.get_u16()
+		texture_height = stream.get_u16()
+		id_hash = stream.get_u16()
+
+		pal_size = COLOR_SIZE * pow(2, bit_depth)
+
+		match clut:
+			CLUT.NONE:
+				pal_size = 0
+			CLUT.HALF:
+				pal_size /= 2
+			CLUT.FULL:
+				pass
+
+	var pointer: int = ADDRESS_HEADER_END + pal_size
+	palette = bin_data.slice(ADDRESS_HEADER_END, pointer)
+
+	var pixel_data: PackedByteArray = bin_data.slice(pointer, bin_data.size())
+
+	match mode:
+		Mode.PALETTE:
+			pixels = []
+		Mode.RAW:
+			pixels = pixel_data
+		Mode.ACPR:
+			pixels = SpriteCompression.decompress_acpr(bin_data)
+		Mode.MODE_5:
+			pixels = SpriteCompression.decompress_mode5(pixel_data)
+		Mode.GGXP:
+			if ggxp_compressed:
+				pixels = SpriteCompression.decompress_ggx(bin_data)
+			else:
+				pixels = pixel_data
+
+	image = Image.create_from_data(width, height, IMAGE_MIPMAPS, IMAGE_FORMAT, pixels)
+	texture = ImageTexture.create_from_image(image)
 
 
 static func get_texture_size(dimension: int) -> int:
@@ -211,209 +447,11 @@ static func load_from_bin(path: String, with_palette: bool) -> BinSprite:
 	return sprite
 
 
-func serialize() -> PackedByteArray:
-	var stream: StreamPeerBuffer = StreamPeerBuffer.new()
-
-	match mode:
-		Mode.RAW:
-			stream.put_u16(MODE_RAW)
-		Mode.PALETTE:
-			stream.put_u16(MODE_PALETTE)
-		_:
-			stream.put_u16(MODE_ACPR)
-
-	match clut:
-		CLUT.NONE:
-			stream.put_u16(CLUT_NONE)
-		CLUT.HALF:
-			stream.put_u16(CLUT_HALF)
-		CLUT.FULL:
-			stream.put_u16(CLUT_FULL)
-
-	# Depth
-	stream.put_u16(bit_depth)
-
-	# Width, height
-	stream.put_u16(width)
-	stream.put_u16(height)
-
-	# Allocated texture width, height
-	stream.put_u16(texture_width)
-	stream.put_u16(texture_height)
-
-	# Hash
-	stream.put_u16(id_hash)
-
-	# Palette
-	if clut != CLUT.NONE:
-		stream.put_data(get_palette())
-
-	# Pixel data
-	match mode:
-		Mode.RAW:
-			stream.put_data(get_pixels())
-		Mode.PALETTE:
-			pass
-		_:
-			stream.put_data(SpriteCompression.compress_acpr(get_pixels(), bit_depth))
-
-	return stream.data_array
-
-
-func deserialize(bin_data: PackedByteArray, is_big_endian: bool) -> void:
-	# Assumes this has already been identified as a sprite
-	var ggxp_compressed: bool = false
-	var pal_size: int
-	var stream: StreamPeerBuffer = StreamPeerBuffer.new()
-	stream.data_array = bin_data
-	stream.big_endian = is_big_endian
-
-	var bin_mode: int = stream.get_u8()
-
-	# GGX Plus sprites
-	if GGXP_MODES.has(bin_mode):
-		mode = Mode.GGXP
-
-		match bin_mode:
-			MODE_GGXP4:
-				bit_depth = DEPTH_4
-			MODE_GGXP8:
-				bit_depth = DEPTH_8
-			_:
-				push_error("BinSprite::deserialize() error! Invalid GGX Plus mode")
-				return
-
-		var ggxp_cc: int = stream.get_u8()
-		
-		# High nibble: compression value
-		match ggxp_cc >> 4:
-			GGXP_UNCOMPRESSED:
-				ggxp_compressed = false
-			GGXP_COMPRESSED:
-				ggxp_compressed = true
-			_:
-				push_error("BinSprite::deserialize() error! Invalid GGX Plus compression")
-				return
-
-		# Low nibble: CLUT value
-		match ggxp_cc & 0xF:
-			GGXP_CLUT_NONE:
-				clut = CLUT.NONE
-			GGXP_CLUT_HALF:
-				clut = CLUT.HALF
-			GGXP_CLUT_FULL:
-				clut = CLUT.FULL
-			_:
-				push_error("BinSprite::deserialize() error! Invalid GGX Plus CLUT")
-				return
-
-		width = stream.get_u16()
-		height = stream.get_u16()
-
-		# Texture size
-		texture_width = get_texture_size(width)
-		texture_height = get_texture_size(height)
-
-		# Hash, palette
-		pal_size = COLOR_SIZE * pow(2, bit_depth)
-
-		match clut:
-			CLUT.NONE:
-				pal_size = 0
-			CLUT.HALF:
-				pal_size /= 2
-			_:
-				pass
-
-		id_hash = hash(bin_data.slice(ADDRESS_HEADER_END + pal_size, bin_data.size()))
-
-	# The rest
-	else:
-		stream.seek(ADDRESS_CLUT)
-		
-		match bin_mode:
-			MODE_RAW:
-				mode = Mode.RAW
-			MODE_ACPR:
-				mode = Mode.ACPR
-			MODE_PALETTE:
-				mode = Mode.PALETTE
-			MODE_5:
-				mode = Mode.MODE_5
-			_:
-				push_error("BinSprite::deserialize() error! Invalid mode")
-				return
-
-		match stream.get_u16():
-			CLUT_NONE:
-				clut = CLUT.NONE
-			CLUT_HALF:
-				clut = CLUT.HALF
-			CLUT_FULL:
-				clut = CLUT.FULL
-			_:
-				push_error("BinSprite::deserialize() error! Invalid CLUT value")
-				return
-
-		bit_depth = stream.get_u16()
-		width = stream.get_u16()
-		height = stream.get_u16()
-		texture_width = stream.get_u16()
-		texture_height = stream.get_u16()
-		id_hash = stream.get_u16()
-
-		pal_size = COLOR_SIZE * pow(2, bit_depth)
-
-		match clut:
-			CLUT.NONE:
-				pal_size = 0
-			CLUT.HALF:
-				pal_size /= 2
-			CLUT.FULL:
-				pass
-
-	var pointer: int = ADDRESS_HEADER_END + pal_size
-	palette = bin_data.slice(ADDRESS_HEADER_END, pointer)
-
-	var pixel_data: PackedByteArray = bin_data.slice(pointer, bin_data.size())
-
-	match mode:
-		Mode.PALETTE:
-			pixels = []
-		Mode.RAW:
-			pixels = pixel_data
-		Mode.ACPR:
-			pixels = SpriteCompression.decompress_acpr(bin_data)
-		Mode.MODE_5:
-			pixels = SpriteCompression.decompress_mode5(pixel_data)
-		Mode.GGXP:
-			if ggxp_compressed:
-				pixels = SpriteCompression.decompress_ggx(bin_data)
-			else:
-				pixels = pixel_data
-
-	image = Image.create_from_data(width, height, IMAGE_MIPMAPS, IMAGE_FORMAT, pixels)
-	texture = ImageTexture.create_from_image(image)
-
-
 func has_palette() -> bool:
 	if clut == CLUT.NONE:
 		return false
 	
 	return !palette.is_empty()
-
-
-func get_palette() -> PackedByteArray:
-	match clut:
-		CLUT.NONE:
-			return []
-		_:
-			return palette
-
-
-func set_palette(new_palette: PackedByteArray) -> void:
-	new_palette.resize(COLOR_SIZE * get_color_count())
-	palette = new_palette
 
 
 func get_color_count() -> int:
@@ -484,5 +522,68 @@ func palette_make_opaque() -> void:
 		palette[COLOR_SIZE * index + 3] = 0xFF
 
 
-func get_pixels() -> PackedByteArray:
-	return pixels
+func get_pixel(x: int, y: int) -> int:
+	x = clampi(x, 0, width - 1)
+	y = clampi(y, 0, height - 1)
+	
+	return pixels[y * height + x]
+
+
+func flip_h() -> void:
+	image.flip_x()
+	pixels = image.get_data()
+	texture = ImageTexture.create_from_image(image)
+
+
+func flip_v() -> void:
+	image.flip_y()
+	pixels = image.get_data()
+	texture = ImageTexture.create_from_image(image)
+
+
+func transform_index(index: int) -> int:
+	if ((index / 8) + 2) % 4 == 0:
+		return index - 8
+	
+	elif ((index / 8) + 3) % 4 == 0:
+		return index + 8
+	
+	return index
+
+
+func transform_index_array(array: PackedByteArray) -> PackedByteArray:
+	var temp_array: PackedByteArray = []
+	
+	for pixel: int in array.size():
+		temp_array.append(transform_index(array[pixel]))
+	
+	return temp_array
+
+
+func transform_rgba_array(array: PackedByteArray) -> PackedByteArray:
+	var temp_array: PackedByteArray = []
+	temp_array.resize(array.size())
+	var color_count: int = array.size() / COLOR_SIZE
+	
+	for index: int in color_count:
+		var new_index: int = transform_index(index)
+		temp_array[COLOR_SIZE * index + 0] = array[COLOR_SIZE * new_index + 0]
+		temp_array[COLOR_SIZE * index + 1] = array[COLOR_SIZE * new_index + 1]
+		temp_array[COLOR_SIZE * index + 2] = array[COLOR_SIZE * new_index + 2]
+		temp_array[COLOR_SIZE * index + 3] = array[COLOR_SIZE * new_index + 3]
+	
+	return temp_array
+
+
+func reindex_pixels() -> void:
+	if bit_depth == DEPTH_4:
+		return
+	
+	pixels = transform_index_array(pixels)
+
+
+func reindex_palette() -> void:
+	if bit_depth == DEPTH_4 || clut == CLUT.NONE:
+		return
+	
+	palette = transform_rgba_array(palette)
